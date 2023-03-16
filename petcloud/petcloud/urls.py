5e0425e315c29ec7e906c8e48867af9890d8e63b
denis <dublin87@mail.ru>
2023-03-17 01:40:53 +0300

    add templates/base.html

---
+++ petcloud/petcloud/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from news.views import *
+from registr.views import *
+from oblako.views import *
+
+from registr.views import SignUp
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('registr/', SignUp.as_view(), name = 'signup'),
+    #path('editprofile/', editprofile, name='editprofile'),
+    path('news_form/', news_add),
+    path('files_add/', filesadd, name='filesadd'),
+    path('profile/<int:pk>/', ProfileView.as_view(), name = 'profile_view'),
+
+]
